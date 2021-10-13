e89522cfcf8c68b7c4cef869bc56307f889107f3
Terrence Cheng - Redso <terrencecheng@redso.com.hk>
2021-10-13 12:27:30 +0800

    chore: remove duplicate

--- roomq_module/src/main/java/com/example/roomq_module/network/RoomStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.roomq_module.network;
-
-public class RoomStatus {
-    public String backend;
-    public String state;
-}
--- roomq_module/src/test/java/com/example/roomq_module/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.roomq_module;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
